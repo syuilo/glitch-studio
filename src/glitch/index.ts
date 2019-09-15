365cd25d1cc6b2fe863da56f614e85f8acb3bc44
syuilo <Syuilotan@yahoo.co.jp>
2019-09-16 03:40:16 +0900

    Update index.ts

--- src/glitch/index.ts
+++ src/glitch/index.ts
@@ -1,28 +1,51 @@
 import * as Jimp from 'jimp';
 
-export async function render(src: string) {
-	const img = await Jimp.default.read(src);
-
-	const out = img.clone();
-
-	console.time('test');
-	function getPx(x, y) {
-		const idx = (img.bitmap.width * y + x) << 2;
-		return [img.bitmap.data[idx + 0], img.bitmap.data[idx + 1], img.bitmap.data[idx + 2]];
-	}
-	function setPx(x, y, rgb) {
-		const idx = (img.bitmap.width * y + x) << 2;
-		out.bitmap.data[idx + 0] = rgb[0];
-		out.bitmap.data[idx + 1] = rgb[1];
-		out.bitmap.data[idx + 2] = rgb[2];
-	}
-	for (let x = 0; x < img.bitmap.width; x++) {
-		for (let y = 0; y < img.bitmap.height; y++) {
-			const [r, g, b] = getPx(x, y);
-			setPx(x, y, [b, g, r]);
+const swap = createFx('swap', (w, h, get, set) => {
+	for (let x = 0; x < w; x++) {
+		for (let y = 0; y < h; y++) {
+			const [r, g, b] = get(x, y);
+			set(x, y, [b, g, r]);
 		}
 	}
-	console.timeEnd('test');
+});
 
-	return out;
+export async function render(src: string) {
+	let img = await Jimp.default.read(src);
+
+	img = swap(img);
+
+	return img;
+}
+
+type Pixel = [number, number, number];
+
+type FX = (
+	width: number, height: number,
+	get: (x: number, y: number) => Pixel, set: (x: number, y: number, color: Pixel) => void,
+	params: Record<string, any>
+) => void;
+
+function createFx(name: string, fx: FX) {
+	return (input) => {
+		const output = input.clone();
+		
+		function get(x: number, y: number) {
+			const idx = (input.bitmap.width * y + x) << 2;
+			return [input.bitmap.data[idx + 0], input.bitmap.data[idx + 1], input.bitmap.data[idx + 2]] as Pixel;
+		}
+
+		function set(x: number, y: number, rgb: Pixel) {
+			const idx = (input.bitmap.width * y + x) << 2;
+			output.bitmap.data[idx + 0] = rgb[0];
+			output.bitmap.data[idx + 1] = rgb[1];
+			output.bitmap.data[idx + 2] = rgb[2];
+		}
+
+		const label = `FX: ${name}`;
+		console.time(label);
+		fx(input.bitmap.width, input.bitmap.height, get, set, {});
+		console.timeEnd(label);
+
+		return output;
+	}
 }
