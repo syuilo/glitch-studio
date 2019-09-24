4daf7b46bf2999828da8a3e2ef2a633ececa4707
syuilo <Syuilotan@yahoo.co.jp>
2019-09-24 16:31:48 +0900

    Update histogram.vue

--- src/components/histogram.vue
+++ src/components/histogram.vue
@@ -39,28 +39,52 @@ export default Vue.extend({
 			ctx.globalCompositeOperation = 'lighter';
 			ctx.lineWidth = 1;
 
+			//#region Draw grid
+			ctx.strokeStyle = 'rgba(255, 255, 255, 0.07)';
+
+			// X axis
+			const xLines = 6;
+			for (let i = 0; i <= xLines; i++) {
+				ctx.beginPath();
+				ctx.moveTo(Math.floor(i * ((this.width - 1) / xLines)) + 0.5, 0);
+				ctx.lineTo(Math.floor(i * ((this.width - 1) / xLines)) + 0.5, this.height);
+				ctx.closePath();
+				ctx.stroke();
+			}
+
+			// Y axis
+			const yLines = 6;
+			for (let i = 0; i <= yLines; i++) {
+				ctx.beginPath();
+				ctx.moveTo(0, Math.floor(i * ((this.height - 1) / yLines)) + 0.5);
+				ctx.lineTo(this.width, Math.floor(i * ((this.height - 1) / yLines)) + 0.5);
+				ctx.closePath();
+				ctx.stroke();
+			}
+			//#endregion
+
 			for (let i = 0; i < 256; i++) {
 				const r = (this.histogram.bins.r[i] / this.histogram.rgbMax) * this.height;
-				ctx.strokeStyle = '#f00';
+				ctx.strokeStyle = `rgb(${i}, 0, 0)`;
 				ctx.beginPath();
-				ctx.moveTo(i, 255);
-				ctx.lineTo(i, Math.max(0, this.height - r));
+				ctx.moveTo(i + 0.5, 255);
+				ctx.lineTo(i + 0.5, Math.max(0, this.height - r));
 				ctx.closePath();
 				ctx.stroke();
 
 				const g = (this.histogram.bins.g[i] / this.histogram.rgbMax) * this.height;
-				ctx.strokeStyle = '#0f0';
+				ctx.strokeStyle = `rgb(0, ${i}, 0)`;
 				ctx.beginPath();
-				ctx.moveTo(i, 255);
-				ctx.lineTo(i, this.height - g);
+				ctx.moveTo(i + 0.5, 255);
+				ctx.lineTo(i + 0.5, this.height - g);
 				ctx.closePath();
 				ctx.stroke();
 
 				const b = (this.histogram.bins.b[i] / this.histogram.rgbMax) * this.height;
-				ctx.strokeStyle = '#00f';
+				ctx.strokeStyle = `rgb(0, 0, ${i})`;
 				ctx.beginPath();
-				ctx.moveTo(i, 255);
-				ctx.lineTo(i, this.height - b);
+				ctx.moveTo(i + 0.5, 255);
+				ctx.lineTo(i + 0.5, this.height - b);
 				ctx.closePath();
 				ctx.stroke();
 			}
