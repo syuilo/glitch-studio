92418ecfe1e03c51b443e82b971d6b4b57829f87
syuilo <4439005+syuilo@users.noreply.github.com>
2026-09-14 22:30:34 +0900

    wip

--- packages/ui/src/components/GsEffectPicker.vue
+++ packages/ui/src/components/GsEffectPicker.vue
@@ -10,8 +10,10 @@
 			<div :class="$style.leftArea">
 			</div>
 			<div :class="$style.rightArea">
-				<div v-for="[k, effect] in Object.entries(fxDefinitions)" :key="k" :class="$style.effect" @click="emit('chosen', effect)">
-					{{ effect.displayName }}
+				<div :class="$style.effects">
+					<button v-for="[k, effect] in Object.entries(fxDefinitions)" :key="k" class="_button" :class="$style.effect" @click="choose(effect)">
+						{{ effect.displayName }}
+					</button>
 				</div>
 			</div>
 		</div>
@@ -39,15 +41,21 @@ const modal = useTemplateRef('modal');
 function close() {
 	modal.value?.close();
 }
+
+function choose(effect: typeof fxDefinitions[keyof typeof fxDefinitions]) {
+	emit('chosen', effect);
+	close();
+}
 </script>
 
 <style module lang="scss">
 .root {
+	display: flex;
 	margin: auto;
 	position: relative;
-	padding: 32px;
-	min-width: 320px;
-	max-width: 480px;
+	width: 1200px;
+	max-width: 100%;
+	height: 100%;
 	box-sizing: border-box;
 	text-align: center;
 	background: var(--THEME-dialog);
@@ -59,18 +67,28 @@ function close() {
 .actions { gap: 6px; }
 
 .body {
+	flex: 1;
 	display: flex;
 	flex-direction: row;
 	gap: 16px;
 }
 .leftArea {
-	flex: 0.4;
+	flex: 0.3;
 }
 .rightArea {
-	flex: 0.6;
+	flex: 0.7;
+	overflow: auto;
+}
+
+.effects {
+	display: grid;
+	grid-template-columns: repeat(auto-fill, minmax(240px, 1fr));
+	gap: 16px;
 }
 
 .effect {
-
+	padding: 8px;
+	background: light-dark(#0001, #fff1);
+	border-radius: 6px;
 }
 </style>
