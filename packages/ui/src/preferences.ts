0deae4a113c3dec9b729203ad9006f701a912db2
syuilo <4439005+syuilo@users.noreply.github.com>
2026-09-15 15:01:23 +0900

    Update preferences.ts

--- packages/ui/src/preferences.ts
+++ packages/ui/src/preferences.ts
@@ -3,89 +3,93 @@ import { customRef, ref, watch, onScopeDispose } from 'vue';
 import { EventEmitter } from 'eventemitter3';
 import { deepEqual } from '@glitch/shared/utility/deep-equal.js';
 import type { Ref } from 'vue';
+import type { WorkspaceDivider } from './types/workspace.ts';
+import type { MenuItem } from './types/menu.ts';
 
 export const PREF_DEF = definePreferences({
-	animation: { default: true },
-	menuStyle: { default: 'auto' },
-	locale: { default: 'en' },
-	animatedBgInPreview: { default: false },
+	animation: { default: () => true },
+	menuStyle: { default: () => 'auto' },
+	locale: { default: () => 'en' },
+	animatedBgInPreview: { default: () => false },
 	workspaceDefinition: {
-		default: {
-			id: 'root',
-			ratio: 1,
-			type: null,
-			direction: 'horizontal',
-			children: [{
-				id: 'c138f76ec1d84ba5b83b0cc3766a1266',
-				ratio: 0.7,
+		default: () => {
+			return {
+				id: 'root',
+				ratio: 1,
 				type: null,
-				direction: 'vertical',
+				direction: 'horizontal',
 				children: [{
-					id: '938e3eedc00d4287885b6894ee3ea8c3',
-					ratio: 0.6,
-					type: null,
-					direction: 'horizontal',
-					children: [{
-						id: '53ef52d7a5a44b41ba34a0491b4f5d8e',
-						ratio: 0.25,
-						type: null,
-						direction: 'vertical',
-						children: [{
-							id: '0f34c5f4c9cb449683c7f1281851b759',
-							ratio: 0.33,
-							type: 'histogram',
-						}, {
-							id: 'b3d6059aaa554ae79441286cd2beb685',
-							ratio: 0.33,
-							type: 'audioWaveform',
-						}, {
-							id: '47edf72197d94d28b6b2811bfecc97e5',
-							ratio: 0.33,
-							type: 'stats',
-						}],
-					}, {
-						id: '5ec0a586d9654755acdaa2ae6837f28e',
-						ratio: 0.75,
-						type: 'preview',
-					}],
-				}, {
-					id: '8aec4dd7bf82460eba420680fda4f652',
-					ratio: 0.4,
+					id: 'c138f76ec1d84ba5b83b0cc3766a1266',
+					ratio: 0.7,
 					type: null,
 					direction: 'vertical',
 					children: [{
-						id: 'ba8f8efaa9a54a109340b2f3e329cda2',
-						ratio: 0.5,
+						id: '938e3eedc00d4287885b6894ee3ea8c3',
+						ratio: 0.6,
 						type: null,
 						direction: 'horizontal',
 						children: [{
-							id: '251858d938b0448aafd613b73c7e352c',
+							id: '53ef52d7a5a44b41ba34a0491b4f5d8e',
 							ratio: 0.25,
-							type: 'audioSpectrogram',
+							type: null,
+							direction: 'vertical',
+							children: [{
+								id: '0f34c5f4c9cb449683c7f1281851b759',
+								ratio: 0.33,
+								type: 'histogram',
+							}, {
+								id: 'b3d6059aaa554ae79441286cd2beb685',
+								ratio: 0.33,
+								type: 'audioWaveform',
+							}, {
+								id: '47edf72197d94d28b6b2811bfecc97e5',
+								ratio: 0.33,
+								type: 'stats',
+							}],
 						}, {
-							id: '136c0ccc916c438787756c07da414be7',
-							ratio: 0.25,
-							type: 'waveformHorizontal',
-						}, {
-							id: 'ca5eba2936d4423891377972de6a41f5',
-							ratio: 0.25,
-							type: 'waveformVertical',
-						}, {
-							id: '15bd089777d440a0baaf2953c8020e24',
-							ratio: 0.25,
-							type: 'players',
+							id: '5ec0a586d9654755acdaa2ae6837f28e',
+							ratio: 0.75,
+							type: 'preview',
 						}],
 					}, {
-						id: '2d9d8a38ce9a4d24bc70a500f892f124',
-						ratio: 0.5,
-						type: 'timeline',
+						id: '8aec4dd7bf82460eba420680fda4f652',
+						ratio: 0.4,
+						type: null,
+						direction: 'vertical',
+						children: [{
+							id: 'ba8f8efaa9a54a109340b2f3e329cda2',
+							ratio: 0.5,
+							type: null,
+							direction: 'horizontal',
+							children: [{
+								id: '251858d938b0448aafd613b73c7e352c',
+								ratio: 0.25,
+								type: 'audioSpectrogram',
+							}, {
+								id: '136c0ccc916c438787756c07da414be7',
+								ratio: 0.25,
+								type: 'waveformHorizontal',
+							}, {
+								id: 'ca5eba2936d4423891377972de6a41f5',
+								ratio: 0.25,
+								type: 'waveformVertical',
+							}, {
+								id: '15bd089777d440a0baaf2953c8020e24',
+								ratio: 0.25,
+								type: 'players',
+							}],
+						}, {
+							id: '2d9d8a38ce9a4d24bc70a500f892f124',
+							ratio: 0.5,
+							type: 'timeline',
+						}],
 					}],
+				}, {
+					id: '441518aeb37940b2af7fb0027fd530a9',
+					ratio: 0.3,
+					type: 'nodesEditor',
 				}],
-			}, {
-				id: '441518aeb37940b2af7fb0027fd530a9',
-				ratio: 0.3,
-				type: 'nodesEditor',
-			}],
+			} as WorkspaceDivider;
 		},
 	},
 });
@@ -97,7 +101,7 @@ type DefaultValues = {
 type ValueOf<K extends keyof PREF> = DefaultValues[K];
 
 type PreferencesDefinitionRecord<Default> = {
-	default: Default;
+	default: () => Default;
 };
 
 export type PreferencesDefinition = Record<string, PreferencesDefinitionRecord<any>>;
@@ -116,16 +120,6 @@ export function definePreferences<T extends Record<string, unknown>>(x: {
 	[K in keyof T]: PreferencesDefinitionRecord<T[K]>
 } {
 	return x;
-}
-
-export function getInitialPrefValue<K extends keyof PREF>(k: K): ValueOf<K> {
-	const _default = PREF_DEF[k].default;
-	if (typeof _default === 'function') { // factory
-		return _default() as ValueOf<K>;
-	} else {
-		// 参照渡しになるのを防ぐためclone
-		return deepClone(_default as unknown as ValueOf<K>);
-	}
 }
 
 export class PreferencesManager extends EventEmitter<PreferencesManagerEvents> {
@@ -231,7 +225,7 @@ export class PreferencesManager extends EventEmitter<PreferencesManagerEvents> {
 		const states = {} as { [K in keyof PREF]: ValueOf<K> };
 		for (const _key in PREF_DEF) {
 			const key = _key as keyof PREF;
-			(states[key] as any) = getInitialPrefValue(key);
+			(states[key] as any) = PREF_DEF[key].default();
 		}
 
 		return states;
@@ -248,7 +242,7 @@ export class PreferencesManager extends EventEmitter<PreferencesManagerEvents> {
 			text: i18n.ts.resetToDefaultValue,
 			danger: true,
 			action: () => {
-				this.commit(key, getInitialPrefValue(key));
+				this.commit(key, PREF_DEF[key].default());
 			},
 		}];
 	}
