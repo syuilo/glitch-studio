a9dcd53094c95dcdfeafcba3f2308e23eee3bb59
syuilo <4439005+syuilo@users.noreply.github.com>
2026-09-20 18:58:10 +0900

    wip

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import * as Path from 'node:path';
-import packageJson from './package.json' with { type: 'json' };
+import packageJson from '../../package.json' with { type: 'json' };
 
 // https://vitejs.dev/config/
 export default defineConfig({
