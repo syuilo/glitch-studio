a9d5a19924aea42978cc058040f493e864b4e529
syuilo <4439005+syuilo@users.noreply.github.com>
2026-09-26 09:52:49 +0900

    Update types.ts

--- packages/shared/src/visual-module/types.ts
+++ packages/shared/src/visual-module/types.ts
@@ -1,6 +1,6 @@
 import type { TextureDataType } from '../data-type.ts';
 import type { EffectDefinition } from '../effect/effect-definition.ts';
-import type { ParameterDefinition, ParameterDefinition_Any, ParameterDefinition_Array, ParameterDefinition_Struct } from '../parameter.ts';
+import type { ParameterDefinition } from '../parameter.ts';
 import type { FitMode, AutomationGraph, ParameterBinding, WrapMode } from '../types.ts';
 
 export type VisualModuleEffectNode = {
